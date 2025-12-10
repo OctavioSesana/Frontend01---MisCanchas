798a032b53fc6f0d6c0e34f2e8c2c4525d355031
OctavioSesana <osesana@frro.utn.edu.ar>
2025-12-10 11:35:58 -0300

    hola

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "https://backend01-miscanchas-production.up.railway.app/api" 
+  apiUrl: "https://backend01-miscanchas-production.up.railway.app" 
   // ⚠️ OJO: Agregué "/api" al final porque vi en tu código que tus rutas empiezan con /api (ej: /api/canchas).
   // Si tu código en los servicios ya agrega el "/api", bórralo de aquí.
 };
