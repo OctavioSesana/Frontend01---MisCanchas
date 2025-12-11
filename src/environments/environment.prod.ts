2c620576dee86e62da66d83f72845874d917c8e8
OctavioSesana <164179598+OctavioSesana@users.noreply.github.com>
2025-12-11 10:43:19 -0300

    Update API URL for production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // ⚠️ PON LA URL DE RAILWAY AQUÍ TAMBIÉN
-  apiUrl: 'https://backend01-miscanchas-production.up.railway.app'
+  apiUrl: 'https://backend01-miscanchas-production.up.railway.app/api'
 };
